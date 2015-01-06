f300222e4798de1ff6bbcc830be2245e4b84c4b8
monchan89 <monchan89@gmail.com>
2015-01-07 00:54:55 +0900

    Test a push with basic rails

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-dev
-===
+++ dev +
+       |
+       + rails +
+               |
+	       + helloworld
---
+++ helloworld/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = '854278738324f03630bbad7c5eb39d2817a475abf22a7b4e95c3f5aa18a0de51253958082c6cc81ba54fa8aff64348eb3d8575cddaf4965f61df747fad6ad9b7'
