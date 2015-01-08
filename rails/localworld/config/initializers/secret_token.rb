1ef9c382e3dc8df9daee7d7d683987ddd92fa935
monchan89 <monchan89@gmail.com>
2015-01-09 00:57:37 +0900

    some samples that hopefully works

---
+++ rails/localworld/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Localworld::Application.config.secret_key_base = '95474b515ef1447a957eb8639f9c83cd36fe5cb5bacf651605d02ae68b2bf0dc8a8bd4762f6ad505ae3984895830a4b0a93f5bba4ea9e4d7973489051be2bd7b'
